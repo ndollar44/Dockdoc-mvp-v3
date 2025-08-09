e41215f6763d74f5766d895418393737fa1e5d1d
ndollar44 <ndollar44@gmail.com>
2025-08-08 23:29:26 -0400

    Create sampleData.ts

---
+++ src/data/sampleData.ts
@@ -0,0 +1,5 @@
+export const partsList = [
+  { id: 1, name: "Dock Leveler Spring", price: 120 },
+  { id: 2, name: "Hydraulic Pump", price: 450 },
+  { id: 3, name: "Control Box", price: 200 },
+];
